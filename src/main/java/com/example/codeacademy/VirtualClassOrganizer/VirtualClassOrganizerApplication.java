6dff73abef18400b52228fa895d99c89f3856e01
Borjan <brainstererrors@gmail.com>
2021-01-31 11:56:14 +0100

    initial project

---
+++ src/main/java/com/example/codeacademy/VirtualClassOrganizer/VirtualClassOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeacademy.VirtualClassOrganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualClassOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualClassOrganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codeacademy/VirtualClassOrganizer/VirtualClassOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeacademy.VirtualClassOrganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualClassOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
